6106fd5fa5ead6c1fcd5de5ade5fff1d412cbd7b
Ayaka-Katsuragi <38163555+Ayaka-Katsuragi@users.noreply.github.com>
2019-03-04 04:35:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
 	console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-	bot.user.setActivity("Re downloading database db", {type: "PLAYING"});
+	bot.user.setActivity("connecting to telegrad.co.jp", {type: "PLAYING"});
 });
 
 bot.on("guildMemberAdd", async member => {
